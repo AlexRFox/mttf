c18e4890868888edc49d63145bbc3b5426127e8a
Alex Willisson <atw@mit.edu>
2011-12-31 14:53:10 -0500

    working on recurrence parsing

--- mttf-add.c
+++ mttf-add.c
@@ -1,14 +1,68 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
 struct event {
-	char *title, *description, *recur;
-	int year, month, day;
+	char *script, *title, *date, *repeat, *ical_recur;
 };
+
+struct event ev;
+
+void
+usage (void)
+{
+	printf ("bad usage\n");
+	exit (1);
+}
+
+char *
+parse_recur (void)
+{
+	int val;
+	char unit;
+
+	sscanf (ev.repeat, "%d%c", &val, &unit);
+
+	printf ("%d, %c\n", val, unit);
+
+	return (strdup (""));
+}
 
 int
 main (int argc, char **argv)
 {
-	
+	int c;
+
+	while ((c = getopt (argc, argv, "")) != EOF) {
+		switch (c) {
+		default:
+			break;
+		}
+	}
+
+	if (optind + 3 > argc)
+		usage ();
+
+	ev.script = argv[optind++];
+	ev.title = argv[optind++];
+	ev.date = argv[optind++];
+
+	ev.repeat = "";
+
+	if (optind < argc) {
+		ev.repeat = argv[optind++];
+		ev.ical_recur = parse_recur ();
+	}
+
+	if (optind != argc)
+		usage ();
+
+	printf ("script: %s\n", ev.script);
+	printf ("title: %s\n", ev.title);
+	printf ("date: %s\n", ev.date);
+	if (*ev.ical_recur)
+		printf ("recurrence: %s\n", ev.ical_recur);
 
 	return (0);
 }
