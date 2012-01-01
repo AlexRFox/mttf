ea7e9958375c35d6d6a7808e6d5f0c107968d023
Alex Willisson <atw@mit.edu>
2011-12-31 23:07:40 -0500

    working on parsing json into birthday

--- Makefile
+++ Makefile
@@ -1,11 +1,15 @@
 CFLAGS = -g -Wall
 LIBS = -lm
 
-all: mttf-add
+all: mttf-add birthday
 
 MTTF_ADD_OBJS = mttf-add.o json.o
 mttf-add: $(MTTF_ADD_OBJS)
 	$(CC) $(CFLAGS) -o mttf-add $(MTTF_ADD_OBJS) -lm
+
+BIRTHDAY_OBJS = birthday.o json.o
+birthday: $(BIRTHDAY_OBJS)
+	$(CC) $(CFLAGS) -o birthday $(BIRTHDAY_OBJS) -lm
 
 clean:
 	rm -f *~ *.o 
--- birthday.c
+++ birthday.c
@@ -1,9 +1,38 @@
 #include <stdio.h>
+#include <string.h>
+#include "json.h"
+
+void
+usage (void)
+{
+	printf ("usage: birthday json_obj_input_file\n");
+}
 
 int
 main (int argc, char **argv)
 {
-	
+	FILE *f;
+	char *filename, buf[1000], jsonstr[10000];
+	struct json *json;
+
+	if (argc != 2)
+		usage ();
+
+	filename = argv[1];
+
+	if ((f = fopen (filename, "r")) == NULL) {
+		fprintf (stderr, "error opening file\n");
+		return (1);
+	}
+
+	memset (&jsonstr, 0, sizeof jsonstr);
+	while (fgets (buf, sizeof buf, f) != NULL) {
+		sprintf (jsonstr, "%s%s", jsonstr, buf);
+	}
+
+	json = json_decode (jsonstr);
+
+
 
 	return (0);
 }
